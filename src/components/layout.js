9c8aed0cbba7c36738c815abd5f0a564343ae8fb
james <jpratt20@ischool.org>
2020-09-10 09:35:00 -1000

    footer completed

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Link from "gatsby-link"
+import { Link as Scroll } from "react-scroll"
 
 import Header from "./header"
 
@@ -33,19 +34,59 @@ const Layout = ({ children }) => {
       >
         <main> {children} </main>
         <footer>
-          <div></div>
-          <div>
-            <div className="socials"></div>
+          <div className="kcsc-logo logobox"></div>
+          <div className="footer-item socialbox">
+            <h3>Social Media</h3>
             <div>
-              <Link to="/contact">Contact</Link>
+              <a href="https://www.instagram.com/kauaicsc">insta</a>
+              <a href="https://www.facebook.com/kauaicsc">facebook</a>
+              <a href="https://twitter.com/kauaicsc">twitter</a>
+              <a href="https://www.linkedin.com/company/64640520/admin/">
+                LinkedIn
+              </a>
             </div>
-            <div>
-              Our Adress: West Kaua`i Technology Center, Unit 102A, 9565
-              Kaumuali`i Highway, P.O. Box 60, Waimea, Hawaii 96796,
-            </div>
-            <div>Phone</div>
           </div>
-          <div>Copyright © 2018 KAUAICSC - All Rights Reserved.</div>
+          <div className="footer-item pagesbox">
+            <h3>Pages</h3>
+            <div>
+              <Link to="/">home</Link>
+              <Link to="/about">about</Link>
+              <Link to="/internship">internship</Link>
+              <Link to="contact">contact</Link>
+              <Link to="/projects">projects</Link>
+              <Link to="/donate">donate</Link>
+            </div>
+          </div>
+
+          <div className="footer-item contactbox">
+            <h3>Contact Information</h3>
+            <p>
+              Address: West Kaua`i Technology Center, Unit 102A, 9565 Kaumuali`i
+              Highway, P.O. Box 60, Waimea, Hawaii 96796
+            </p>
+            <p>
+              Phone: <a href="tel:+1(808)645-6017">(808)645-6017</a>
+            </p>
+            <p>
+              Email: <a href="mailto:info@kauaicsc.org">info@kauaicsc.org</a>
+            </p>
+            <p>
+              Contact Form: <Link to="/contact">Contact</Link>
+            </p>
+          </div>
+          <Scroll
+            className="footer-top totopbox"
+            spy={true}
+            smooth="true"
+            offset={-70}
+            duration={500}
+            to="top"
+          >
+            back to top
+          </Scroll>
+          <div className="copyright">
+            Copyright © 2018 KAUAICSC - All Rights Reserved.
+          </div>
         </footer>
       </div>{" "}
     </>
--- src/modules/nav.js
+++ src/modules/nav.js
@@ -4,30 +4,30 @@ import { Link } from "gatsby"
 import "../styles/main.scss"
 
 const Nav = () => (
-  <nav>
+  <nav id="top">
     <h1 className="main-logo">
       {" "}
       <Link to="/">
-        <div className="kcsc-logo"></div>
-      </Link>
-    </h1>
+        <div className="kcsc-logo"> </div>{" "}
+      </Link>{" "}
+    </h1>{" "}
     <div className="alternate-links">
       <h3>
-        <Link to="/about">about</Link>
-      </h3>
+        <Link to="/about"> about </Link>{" "}
+      </h3>{" "}
       <h3>
-        <Link to="/internship">internship</Link>
-      </h3>
+        <Link to="/internship"> internship </Link>{" "}
+      </h3>{" "}
       <h3>
-        <Link to="/contact">contact</Link>
-      </h3>
+        <Link to="/contact"> contact </Link>{" "}
+      </h3>{" "}
       <h3>
-        <Link to="/projects">projects</Link>
-      </h3>
+        <Link to="/projects"> projects </Link>{" "}
+      </h3>{" "}
       <h3 className="btn">
-        <Link to="/donate">donate</Link>
-      </h3>
-    </div>
+        <Link to="/donate"> donate </Link>{" "}
+      </h3>{" "}
+    </div>{" "}
   </nav>
 )
 
